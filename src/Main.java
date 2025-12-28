7b7b6ba9a42a526817e90d63ab785c51f1b4735b
Rikza <maryamrikza@gmail.com>
2025-12-28 23:55:33 +0500

    added a menu

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Study Planner started");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("~Study Planner~");
+
+        System.out.println("\n menu");
+        System.out.println("1. add subjects");
+        System.out.println("2. add tasks");
+        System.out.println("3. add due dates");
+        System.out.println("4. view tasks and due date");
+        System.out.println("5. exit");
+
+        System.out.print("enter choice: ");
+        int choice = scanner.nextInt();
+        System.out.println(choice);
     }
 }
