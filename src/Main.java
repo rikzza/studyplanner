ee616f09b12de989b11ee5334112bb844a3aecfb
Rikza <maryamrikza@gmail.com>
2026-01-02 23:57:53 +0500

    add .gitignore and updated cli studyplanner code

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,56 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
-public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("~Study Planner~");
-
+class Menu
+{
+    public static void showMenu()
+    {
         System.out.println("\n menu");
         System.out.println("1. add subjects");
         System.out.println("2. add tasks");
         System.out.println("3. add due dates");
         System.out.println("4. view tasks and due date");
         System.out.println("5. exit");
+    }
+}
 
-        System.out.print("enter choice: ");
-        int choice = scanner.nextInt();
-        System.out.println(choice);
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ArrayList <String> subjects = new ArrayList<>();
+        ArrayList <String> tasks = new ArrayList<>();
+
+        System.out.println("~Study Planner~");
+
+        while (true)
+        {
+            Menu.showMenu();
+            System.out.print("enter choice: ");
+            int choice = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (choice) {
+                case 1: {
+                    System.out.println("enter subject name: ");
+                    String subject = scanner.nextLine();
+                    subjects.add(subject);
+                }
+                break;
+
+                case 2: {
+                    System.out.println("enter tasks to do: ");
+                    String task = scanner.nextLine();
+                    tasks.add(task);
+                }
+                break;
+
+                case 3: {
+                    System.out.println("view subjects and tasks: ");
+                    System.out.println("subjects added: " + subjects);
+                    System.out.println("tasks added" + tasks);
+
+                }break;
+            }
+        }
     }
 }
