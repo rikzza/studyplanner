0f8cf551753893c01c6b9bf456d8f39bd8fb56eb
Rikza <maryamrikza@gmail.com>
2026-01-03 21:46:45 +0500

    fixed subjects and tasks and added due dates.

--- src/Main.java
+++ src/Main.java
@@ -5,22 +5,22 @@ class Menu
 {
     public static void showMenu()
     {
-        System.out.println("\n menu");
-        System.out.println("1. add subjects");
-        System.out.println("2. add tasks");
-        System.out.println("3. add due dates");
-        System.out.println("4. view tasks and due date");
-        System.out.println("5. exit");
+        System.out.println("\n Menu");
+        System.out.println("1. Add subjects");
+        System.out.println("2. Add tasks");
+        System.out.println("3. View subjects and tasks");
+        System.out.println("4. Exit");
     }
 }
 
 public class Main {
-    public static void main(String[] args) {
+     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         ArrayList <String> subjects = new ArrayList<>();
         ArrayList <String> tasks = new ArrayList<>();
+        ArrayList <String> dueDates = new ArrayList<>();
 
-        System.out.println("~Study Planner~");
+        System.out.println("\n~Study Planner~");
 
         while (true)
         {
@@ -29,27 +29,87 @@ public class Main {
             int choice = scanner.nextInt();
             scanner.nextLine();
 
-            switch (choice) {
-                case 1: {
-                    System.out.println("enter subject name: ");
-                    String subject = scanner.nextLine();
-                    subjects.add(subject);
+            switch (choice)
+            {
+                case 1:
+                {
+                    while(true)
+                    {
+                        System.out.println("enter subject (enter 0 to stop): ");
+                        String subject = scanner.nextLine();
+
+                        if (subject.equals("0"))
+                        {
+                            System.out.println("done!");
+                            break;
+                        }
+                        subjects.add(subject);
+                    }
                 }
                 break;
 
-                case 2: {
-                    System.out.println("enter tasks to do: ");
-                    String task = scanner.nextLine();
-                    tasks.add(task);
+                case 2:
+                {
+                    while(true)
+                    {
+                        System.out.println("enter tasks to do (enter 0 to stop): ");
+                        String task = scanner.nextLine();
+
+                        if(task.equals("0"))
+                        {
+                            System.out.println("done!");
+                            break;
+                        }
+
+                        System.out.println("due date for task? ");
+                        String dueDate = scanner.nextLine();
+
+                        tasks.add(task);
+                        dueDates.add(dueDate);
+                    }
                 }
                 break;
 
-                case 3: {
+                case 3:
+                {
                     System.out.println("view subjects and tasks: ");
-                    System.out.println("subjects added: " + subjects);
-                    System.out.println("tasks added" + tasks);
 
-                }break;
+                    System.out.println("subjects added: ");
+                    if (subjects.isEmpty())
+                    {
+                        System.out.println("none");
+                    }
+                    else
+                    {
+                        for (int i = 0; i<subjects.size(); i++) {
+                            System.out.println((i + 1) + ". " + subjects.get(i));
+                        }
+                    }
+
+                    System.out.println("tasks and their due dates: ");
+                    if (tasks.isEmpty())
+                    {
+                        System.out.println("none");
+                    }
+                    else
+                    {
+                        for (int i = 0; i<tasks.size(); i++) {
+                            System.out.println((i + 1) + ". " + tasks.get(i) + " (Due: " + dueDates.get(i) +" )");
+                        }
+                    }
+                }
+                break;
+
+                case 4:
+                {
+                    System.out.println("exiting program...");
+                    return;
+                }
+
+                default:
+                {
+                    System.out.println("Wrong choice. Try again");
+                }
             }
         }
     }
